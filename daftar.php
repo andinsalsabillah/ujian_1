73aac072504bb4c8143b76baa84bacb5bdab19f2
Andine salsabillah <andinsalsabillah3@gmail.com>
2026-05-25 09:58:14 +0700

    akhir lagi

--- daftar.php
+++ daftar.php
@@ -1,32 +1,46 @@
+<?php
+include "koneksi.php";
+
+if (isset($_POST['submit'])) {
+
+    $username = $_POST['username'];
+    $email    = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO tb_users(username,email,password)
+            VALUES('$username','$email','$password')";
+
+    $query = mysqli_query($koneksi, $sql);
+
+    if ($query) {
+        echo "<script>alert('Pendaftaran berhasil');</script>";
+    } else {
+        echo "<script>alert('Pendaftaran gagal');</script>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>Form Pendaftaran</title>
+    <title>Pendaftaran</title>
 </head>
 <body>
 <center>
     <h2>Form Pendaftaran</h2>
 
-        <p>
-            <label for="username">Username:</label><br>
-            <input type="text" id="username" name="username" required>
-        </p>
+    <form action="daftar.php" method="POST">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
 
-        <p>
-            <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email" required>
-        </p>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
 
-        <p>
-            <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password" required>
-        </p>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
 
-        <p>
-            <button type="submit" name="daftar">Daftar</button>
-        </p>
-
+        <button type="submit" name="submit">Daftar</button>
     </form>
 </center>
 </body>
